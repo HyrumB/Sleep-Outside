62e1b86257e3c8f64f9ab96da6f3485178f2cd79
Brenleigh Killpack <kil21008@byui.edu>
2024-05-03 18:26:33 -0600

    ran lint and format now custom alert done

--- src/js/alertModule.js
+++ src/js/alertModule.js
@@ -1,28 +1,26 @@
-fetch('../json/alerts.json')
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then((alerts) => {
-        if (alerts.length > 0) {
-            const alertSection = document.createElement('section');
-            alertSection.classList.add('alert-list');
-            alerts.forEach((alert) => {
-                const alertElement = document.createElement('p');
-                alertElement.textContent = alert.message;
-                alertElement.style.backgroundColor = alert.background;
-                alertElement.style.color = alert.color;
-                alertSection.appendChild(alertElement);
+fetch("../json/alerts.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((alerts) => {
+    if (alerts.length > 0) {
+      const alertSection = document.createElement("section");
+      alertSection.classList.add("alert-list");
+      alerts.forEach((alert) => {
+        const alertElement = document.createElement("p");
+        alertElement.textContent = alert.message;
+        alertElement.style.backgroundColor = alert.background;
+        alertElement.style.color = alert.color;
+        alertSection.appendChild(alertElement);
 
-            const mainElement = document.querySelector('main');
-            mainElement.prepend(alertSection);
-            })
-        } else {
-            console.log('No alerts found');
-        }
-    })
-    .catch((error) => {
-        console.error('Fetch error:', error);
-    });
+        const mainElement = document.querySelector("main");
+        mainElement.prepend(alertSection);
+      });
+    }
+  })
+  .catch((error) => {
+    throw new Error("Fetch error:", error);
+  });
