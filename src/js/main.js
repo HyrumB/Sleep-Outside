a223d4b42ecaae73324963073d1cb65592389080
Brenleigh Killpack <kil21008@byui.edu>
2024-05-17 09:59:31 -0600

    linked alert.mjs to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
-
 // main.js 
 import ProductList from "./components/ProductList.svelte";
+import { generateAlerts } from "./alert.mjs";
+
+generateAlerts();
 
 new ProductList({
     target: document.querySelector(".products"),
