14e4db3af8d19623d76dd95134564a2d0aaa1ded
HyrumB <hyrumbullock@gmail.com>
2024-05-31 10:41:44 -0600

    added new files

---
+++ src/js/product-list.js
@@ -0,0 +1,10 @@
+import ProductList from "./components/ProductList.svelte";
+import { getQueryString } from "./utils.mjs";
+
+const itemCategory = getQueryString("itemType");
+console.log(itemCategory);
+
+new ProductList({
+    target: document.querySelector(".products"),
+    props: { category:  itemCategory},
+});
