3608602423e600fb1ce564bfa469c230e1b325f2
HyrumB <hyrumbullock@gmail.com>
2024-06-04 08:47:07 -0600

    got other categorys to add to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
 import { findProductById } from "./productData.mjs";
 import { addProductToCart } from "./productDetail.mjs";
 import { cartAnimation } from "./cartAnimation.mjs";
+import { getQueryString } from "./utils.mjs";
 
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
+  const itemType = getQueryString("itemType");
+  const product = await findProductById(e.target.dataset.id, itemType);
   addProductToCart(product);
 
   cartAnimation();
