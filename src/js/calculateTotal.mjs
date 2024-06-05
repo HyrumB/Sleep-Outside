88891857ee89a5b9d84f0f396b9ff3bdfc05bab6
Brenleigh Killpack <kil21008@byui.edu>
2024-06-05 12:47:03 -0600

    fixed small bug with cart total

--- src/js/calculateTotal.mjs
+++ src/js/calculateTotal.mjs
@@ -1,21 +1,35 @@
 import { getLocalStorage } from "./utils.mjs";
 
 export function calculateTotal() {
-    const cartItems = getLocalStorage("so-cart");
-    const validCartItems = cartItems.filter((item) => item !== null);
-  
-    if (validCartItems.length > 0) {
-      const cartFooter = document.createElement("div");
-      cartFooter.classList.add("cart-footer");
-  
-      const total = validCartItems.reduce((acc, item) => acc + item.quantity * item.FinalPrice, 0);
-  
+  const cartItems = getLocalStorage("so-cart");
+  const validCartItems = cartItems.filter((item) => item !== null);
+
+  const cartFooter = document.querySelector(".cart-footer");
+
+  if (validCartItems.length > 0) {
+    let total = validCartItems.reduce(
+      (acc, item) => acc + item.quantity * item.FinalPrice,
+      0
+    );
+
+    if (cartFooter) {
+      cartFooter.querySelector(
+        ".cart-total"
+      ).textContent = `Total: $${total.toFixed(2)}`;
+    } else {
+      const newCartFooter = document.createElement("div");
+      newCartFooter.classList.add("cart-footer");
+
       const totalParagraph = document.createElement("p");
       totalParagraph.classList.add("cart-total");
       totalParagraph.textContent = `Total: $${total.toFixed(2)}`;
-  
-      cartFooter.appendChild(totalParagraph);
-  
-      document.querySelector("main").appendChild(cartFooter);
+
+      newCartFooter.appendChild(totalParagraph);
+      document.querySelector("main").appendChild(newCartFooter);
+    }
+  } else {
+    if (cartFooter) {
+      cartFooter.remove();
     }
   }
+}
--- src/js/removeFromCart.mjs
+++ src/js/removeFromCart.mjs
@@ -14,7 +14,7 @@ export function removeFromCart(event) {
     const newCartItems = validCartItems.filter((item) => item.Id !== itemId);
     setLocalStorage("so-cart", newCartItems);
     renderCartContents();
-    addEventListener("DOMContentLoaded", calculateTotal);
+    calculateTotal();
     cartCount.set(getCartCount());
   }
 }
