da9a5f7b46d833c73537f177b1fcc57ce520d7c4
HyrumB <hyrumbullock@gmail.com>
2024-06-06 08:13:03 -0600

    removed unneeded files

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -3,6 +3,7 @@ import { getQueryString } from "./utils.mjs";
 
 const itemCategory = getQueryString("itemType");
 
+
 new ProductList({
     target: document.querySelector(".products"),
     props: { category:  itemCategory },
