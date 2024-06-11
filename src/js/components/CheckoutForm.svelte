0ab2ba4b5f3bb377dbc87cb7222313460f7730d7
HyrumB <hyrumbullock@gmail.com>
2024-06-11 09:18:28 -0600

    fixed order numbers

--- src/js/components/CheckoutForm.svelte
+++ src/js/components/CheckoutForm.svelte
@@ -3,58 +3,63 @@
   import { calculateTotal } from "../calculateTotal.mjs";
 
   let list = [];
+  let taxPercent = 0.06;
+
   let subtotal = calculateTotal(getLocalStorage("so-cart"));
-  let tax = 0;
-  let shipping = 0;
-  let orderTax = 0;
-  let total = subtotal + tax;
+  let tax = subtotal * taxPercent;
+  let shipping = 10;
+  let total = subtotal + shipping + tax;
+
+  let cart = getLocalStorage("so-cart");
+  const validCart = cart.filter((item) => item !== null);
+  list = validCart.length;
+  shipping += (list -1) * 2;
 </script>
 
-<form >
+<form>
   <fieldset class="customer-details">
     <legend>Shipping</legend>
     <label for="orderDate">Date:</label>
-    <input type="orderDate" id="orderDate" /><br />
+    <input type="orderDate" id="orderDate" required /><br />
 
     <label for="fname">First name:</label>
-    <input type="text" id="fname" /><br />
+    <input type="text" id="fname" required /><br />
 
     <label for="lname">Last name:</label>
-    <input type="text" id="lname" /><br />
+    <input type="text" id="lname" required /><br />
 
     <label for="street">Street:</label>
-    <input type="text" id="street" /><br />
+    <input type="text" id="street" required /><br />
 
     <label for="city">City name:</label>
-    <input type="text" id="city" /><br />
+    <input type="text" id="city" required /><br />
 
     <label for="state">State name:</label>
-    <input type="text" id="state" /><br />
+    <input type="text" id="state" required /><br />
 
     <label for="zip"> zip Code:</label>
-    <input type="text" id="zip" /><br />
+    <input type="text" id="zip" required /><br />
   </fieldset>
 
   <fieldset class="card-details">
     <legend>Payment</legend>
     <label for="cardnumber">credit card:</label>
-    <input type="text" id="cardnumber" /><br />
+    <input type="text" id="cardnumber" required /><br />
 
     <label for="expiration">expiration Date:</label>
-    <input type="text" id="expiration" /><br />
+    <input type="text" id="expiration" required /><br />
 
     <label for="code">security code:</label>
-    <input type="text" id="code" /><br />
+    <input type="text" id="code" required /><br />
   </fieldset>
 
   <fieldset class="checkout-summary">
     <legend>Order Summary</legend>
-    <p>item subtotal ${subtotal}</p>
-    <p>shipping Estimate ${shipping}</p>
+    <p>item subtotal ${subtotal.toFixed(2)}</p>
+    <p>shipping Estimate ${shipping.toFixed(2)}</p>
 
-    <p>tax ${tax}</p>
-    <p>order Tax ${orderTax}</p>
-    <p>order total ${total}</p>
+    <p>tax ${tax.toFixed(2)}</p>
+    <p>order total ${total.toFixed(2)}</p>
   </fieldset>
 
   <button type="submit">Submit</button>
